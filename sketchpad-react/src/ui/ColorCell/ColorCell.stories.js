385c93b89921231add5c7327895df59e5eb9ba3a
Jhon-Crow <jhamish@mail.ru>
2024-10-02 20:31:11 +0300

    add ColorCell

---
+++ sketchpad-react/src/ui/ColorCell/ColorCell.stories.js
@@ -0,0 +1,29 @@
+import { fn } from '@storybook/test';
+import ColorCell from "./ColorCell.jsx";
+import ThemeDecorator from "../../../.storybook/decorators/themeDecorator.jsx";
+import {Theme} from "../../app/const/theme.js";
+
+
+export default {
+  title: 'ui/ColorCell',
+  component: ColorCell,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { initialColor: '#375891' },
+};
+
+export const Light = {
+  args: {
+
+  },
+};
+Light.decorators = [ThemeDecorator(Theme.light)]
+// export const Dark = {
+//   args: {
+//
+//   },
+// };
+// Dark.decorators = [ThemeDecorator(Theme.dark)]
+

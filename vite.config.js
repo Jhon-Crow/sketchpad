860dc94519006bc92d4bc8d65540cb0f3e4b22cf
Jhon-Crow <jhamish@mail.ru>
2024-10-21 21:45:11 +0300

    add site icon

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/canvas-start",
+  base: "./",
   plugins: [
     svgr({
       svgrOptions: {
